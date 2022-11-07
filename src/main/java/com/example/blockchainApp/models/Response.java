b31cd89ec650ce707a69ceebfe22082416c4d5dd
Marc Cobo <marccobo@gmail.com>
2022-11-07 10:44:21 +0100

    Added methods to response model

--- src/main/java/com/example/blockchainApp/models/Response.java
+++ src/main/java/com/example/blockchainApp/models/Response.java
@@ -19,5 +19,42 @@ public class Response {
     public Response() {
     }
 
+    public Response(long id, Question question, String response) {
+        this.id = id;
+        this.question = question;
+        this.response = response;
+    }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "id=" + id +
+                ", question=" + question +
+                ", response='" + response + '\'' +
+                '}';
+    }
 }
