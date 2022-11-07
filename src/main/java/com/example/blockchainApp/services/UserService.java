464acecd4bb69912f6229b0cb632e5bc222e035f
Marc Cobo <marccobo@gmail.com>
2022-11-07 15:32:22 +0100

    Added components

--- src/main/java/com/example/blockchainApp/services/UserService.java
+++ src/main/java/com/example/blockchainApp/services/UserService.java
@@ -1,48 +1,48 @@
-package com.example.blockchainApp.services;
-
-import com.example.blockchainApp.models.User;
-import com.example.blockchainApp.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-@Service
-public class UserService {
-
-    @Autowired
-    private UserRepository repository;
-
-    @Transactional
-    public List<User> getAllUsers() {
-        return repository.findAll();
-    }
-
-    @Transactional
-    public User getUserById(Long id) {
-        return repository.findById(id).get();
-    }
-    // Returns null if the email doesn't exist
-    // else, return the email
-
-
-    @Transactional
-    public String emailExists(String email) {
-        if (email != null) {
-            User user = repository.findUserByEmail(email);
-            if (user != null) {
-                return email;
-            }
-        }
-        return null;
-    }
-
-    @Transactional
-    public User createUser(User user){
-        repository.save(user);
-        return user;
-    }
-
-
-}
+//package com.example.blockchainApp.services;
+//
+//import com.example.blockchainApp.models.User;
+//import com.example.blockchainApp.repositories.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.List;
+//
+//@Service
+//public class UserService {
+//
+//    @Autowired
+//    private UserRepository repository;
+//
+//    @Transactional
+//    public List<User> getAllUsers() {
+//        return repository.findAll();
+//    }
+//
+//    @Transactional
+//    public User getUserById(Long id) {
+//        return repository.findById(id).get();
+//    }
+//    // Returns null if the email doesn't exist
+//    // else, return the email
+//
+//
+//    @Transactional
+//    public String emailExists(String email) {
+//        if (email != null) {
+//            User user = repository.findUserByEmail(email);
+//            if (user != null) {
+//                return email;
+//            }
+//        }
+//        return null;
+//    }
+//
+//    @Transactional
+//    public User createUser(User user){
+//        repository.save(user);
+//        return user;
+//    }
+//
+//
+//}
